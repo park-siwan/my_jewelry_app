7e492211149225a20de8406ea40ff8892d870a0a
park-siwan <pswan3168@gmail.com>
2021-02-12 00:46:21 +0900

    Create TOC key, install an event, and change the state to an event

--- src/App.js
+++ src/App.js
@@ -11,19 +11,47 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:'WEB', sub:'World wide web!'}
+      mode:'welcome',
+      subject:{title:'WEB', sub:'World wide web!'},
+      welcome:{title:'Welcom', desc:'Hello, React!!'},
+      contents: [
+        {id:1, title:'HTML', desc:'HTML is for information'},
+        {id:2, title:'CSS', desc:'CSS is for design'},
+        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      ]
     }
   }
 
  render(){
+   console.log('App render');
+   let _title, _desc = null;
+   if (this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+   } else if (this.state.mode === 'read'){
+      _title = this.state.contents[0].title;
+      _desc = this.state.contents[0].desc;
+   }
    return (
       <div className="App">
-        <Subject 
+        {/* <Subject 
           title = {this.state.subject.title} 
           sub={this.state.subject.sub}>
-        </Subject>
-        <TOC></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        </Subject> */}
+
+        <header>
+        <h1><a href="/" onClick={function(e){
+          console.log(e);
+          e.preventDefault();
+          this.setState({
+            mode:'welcome'
+          });
+        }.bind(this)}>{this.state.subject.title}</a></h1>
+        {this.state.subject.sub}
+        </header>
+
+        <TOC data={this.state.contents}></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
