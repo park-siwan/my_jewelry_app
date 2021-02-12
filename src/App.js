8dfa9d38895d34868bf97d1db1133480e9661982
park-siwan <pswan3168@gmail.com>
2021-02-13 00:41:01 +0900

    Creted : bind, setState, component event

--- src/App.js
+++ src/App.js
@@ -11,7 +11,8 @@ class App extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
+      selected_content_id:2,
       subject:{title:'WEB', sub:'World wide web!'},
       welcome:{title:'Welcom', desc:'Hello, React!!'},
       contents: [
@@ -29,28 +30,38 @@ class App extends React.Component {
       _title = this.state.welcome.title;
       _desc = this.state.welcome.desc;
    } else if (this.state.mode === 'read'){
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
+     //filter()를 사용해도 되지만 커리큘럼상 제외후 반복문 사용
+     let i = 0;
+     while(i < this.state.contents.length){
+       let data = this.state.contents[i];
+       if(data.id === this.state.selected_content_id){
+        _title = data.title;
+        _desc = data.desc;
+        break;
+       }
+        i = i + 1;
+     }
+
    }
    return (
       <div className="App">
-        {/* <Subject 
+        <Subject 
           title = {this.state.subject.title} 
-          sub={this.state.subject.sub}>
-        </Subject> */}
-
-        <header>
-        <h1><a href="/" onClick={function(e){
-          console.log(e);
-          e.preventDefault();
+          sub={this.state.subject.sub}
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
+        </Subject>
+        <TOC 
+         onChangePage={function(id){
           this.setState({
-            mode:'welcome'
+            mode:'read',
+            selected_content_id:Number(id)
           });
-        }.bind(this)}>{this.state.subject.title}</a></h1>
-        {this.state.subject.sub}
-        </header>
-
-        <TOC data={this.state.contents}></TOC>
+        }.bind(this)}
+        data={this.state.contents}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- src/components/Subject.js
+++ src/components/Subject.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 
 class Subject extends Component {
   render(){
+    console.log('Subject render');
     return (
       <header>
-        <h1><a href="/">{this.props.title}</a></h1>
+        <h1><a href="/" onClick={function(e){
+          e.preventDefault();//새로고침 방지
+          this.props.onChangePage();//App컴포넌트에서 
+        }.bind(this)}>{this.props.title}</a></h1>
         {this.props.sub}
       </header>
     );
